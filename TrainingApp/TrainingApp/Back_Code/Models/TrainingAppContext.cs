1991a5349bc3549615cb2b66d7cf56d6000c2325
CristianJT <cjtodorovic@outlook.com>
2016-05-21 17:19:34 -0300

    Add app context

---
+++ TrainingApp/TrainingApp/Back_Code/Models/TrainingAppContext.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace TrainingApp.Back_Code.Models
+{
+    public class TrainingAppContext : DbContext
+    {
+    
+        public TrainingAppContext() : base("name=TrainingAppContext")
+        {
+        }
+
+        public DbSet<Movement> Movements { get; set; }
+        public DbSet<WorkoutMovement> WorkoutMovements { get; set; }
+
+        public System.Data.Entity.DbSet<TrainingApp.Workout> Workouts { get; set; }
+    }
+}
