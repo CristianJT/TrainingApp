f766af878c7e8e16762d2af081bcbaa822276e03
CristianJT <cjtodorovic@outlook.com>
2016-06-12 11:24:31 -0300

    Angular 2 initial configuration and first files

---
+++ TrainingApp/WebApp/app/app.component.ts
@@ -0,0 +1,25 @@
+﻿import { Component } from '@angular/core';
+import { HTTP_PROVIDERS } from '@angular/http';
+
+import { MovimientoService } from './movimientos/movimiento.service';
+import { MovimientosComponent } from './movimientos/movimientos.component';
+
+import './rxjs-operators';
+
+//Decorator to associate metadata (how to create and use this component) with the component class
+@Component({
+    selector: 'my-app',       //represents the component. Create an instance of the component
+    template: `
+        <h1>{{title}}</h1>
+        <movimientos></movimientos>
+    `,
+    directives: [MovimientosComponent],
+    providers: [
+        HTTP_PROVIDERS,
+        MovimientoService
+    ]
+})
+
+export class AppComponent {
+    title = 'Trainning app';
+}
---
+++ TrainingApp/WebApp/app/movimientos/movimientos.component.ts
@@ -0,0 +1,37 @@
+﻿import { Component, OnInit } from '@angular/core';
+
+import { MovimientoService } from './movimiento.service';
+import { Movimiento } from './movimiento';
+
+@Component({
+    selector: 'movimientos',
+    template: `
+        <h2>Movimientos</h2>
+        <ul>
+          <li *ngFor="let movimiento of movimientos">
+            <span>{{movimiento.nombre}} - {{movimiento.tipo_elemento}}</span>
+          </li>
+        </ul>
+    `
+})
+
+export class MovimientosComponent implements OnInit {
+    movimientos: Movimiento[];
+    errorMessage: string;
+    mode = 'Observable';
+
+    constructor(
+        private movimientoService: MovimientoService
+    ) { }
+
+    getMovimientos() {
+        this.movimientoService.getMovimientos()
+            .subscribe(
+            data => this.movimientos = data,
+            error => this.errorMessage = <any>error);
+    }
+
+    ngOnInit() {
+        this.getMovimientos();
+    }
+}
