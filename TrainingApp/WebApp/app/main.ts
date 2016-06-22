92bddda82287c2f1305a9d7c5bc18c2d94fce072
CristianJT <cjtodorovic@outlook.com>
2016-06-21 23:24:22 -0300

    Change route config to HashLocationStrategy

--- TrainingApp/WebApp/app/main.ts
+++ TrainingApp/WebApp/app/main.ts
@@ -1,4 +1,5 @@
 ﻿import { bootstrap } from '@angular/platform-browser-dynamic';
+import { LocationStrategy, HashLocationStrategy } from '@angular/common';
 import * as _ from 'lodash';
 
 import { AppComponent } from './app.component';
@@ -7,5 +8,6 @@ import { APP_ROUTER_PROVIDERS } from './app.routes';
 window._ = _;
 
 bootstrap(AppComponent, [
-    APP_ROUTER_PROVIDERS
+    APP_ROUTER_PROVIDERS,
+    { provide: LocationStrategy, useClass: HashLocationStrategy 
 ]);
